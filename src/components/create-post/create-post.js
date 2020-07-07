282ef2c6fab494204ead483fde22708ad297490b
Aidana Niiazova <aidana.niiazova@gmail.com>
2020-07-08 04:32:11 +0600

    post creation form

--- src/components/create-post/create-post.js
+++ src/components/create-post/create-post.js
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 import './style/create-post.css';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import { Row, Col, Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
 const CreatePost = () => {
     return (
         <div className={"create-post"}>
@@ -45,9 +45,48 @@ const CreatePost = () => {
                     </FormGroup>
                 </Form>
             </div>
-            <div className={"create-post__date"}>2</div>
-            <div className={"create-post__title"}>3</div>
-            <div className={"create-post__description"}>3</div>
+            <div className={"create-post__date"}>
+                <Label>Select the date of the delivery</Label>
+                <Row form>
+                    <Col md={2}>
+                        <FormGroup>
+                            <Input type="select">
+                                <option>Day</option>
+                            </Input>
+                        </FormGroup>
+                    </Col>
+                    <Col md={2}>
+                        <FormGroup>
+                            <Input type="select">
+                                <option>Month</option>
+                            </Input>
+                        </FormGroup>
+                    </Col>
+                    <Col md={2}>
+                        <FormGroup>
+                            <Input type="select">
+                                <option>Year</option>
+                            </Input>
+                        </FormGroup>
+                    </Col>
+                </Row>
+            </div>
+            <div className={"create-post__title"}>
+                <Form>
+                    <FormGroup>
+                        <Label>Please, add post title:</Label>
+                        <Input type="textarea" name="text" />
+                    </FormGroup>
+                </Form>
+            </div>
+            <div className={"create-post__description"}>
+                <Form>
+                    <FormGroup>
+                        <Label>Please, add post body:</Label>
+                        <Input type="textarea" name="text" />
+                    </FormGroup>
+                </Form>
+            </div>
             <div className={"create-post__photos"}>4</div>
             <div className={"create-post__buttons"}>5</div>
         </div>
